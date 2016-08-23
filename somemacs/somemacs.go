280d4bab9e72a1d0870f8de725dc7a544d97d3b4
Gavin Grover <gvgrover@yahoo.com>
2016-08-24 07:46:32 +0800

    initial load
    initial load

---
+++ somemacs/somemacs.go
@@ -0,0 +1,22 @@
+// Copyright 2016 The Groo Authors. All rights reserved.
+// Use of this source code is governed by the same BSD-style
+// license as Go that can be found in the LICENSE file.
+
+package somemacs
+
+import (
+	"go/ast"
+	"github.com/grolang/gro/macro"
+)
+
+type Struct struct{}
+
+func (m Struct) Init(p interface{}) {
+}
+
+func (m Struct) Main(p macro.Parser) ast.Stmt {
+	s:= &ast.EmptyStmt{Semicolon: p.Pos(), Implicit: true}
+	p.Next()
+	return s
+}
+
